e77b5b712db8b08563cfc6c917357f4cf3c8277c
Adoo2401 <abdullahrehmani230@gmail.com>
2023-08-09 20:55:37 +0500

    createdDashboard

--- app/api/getSheet/route.ts
+++ app/api/getSheet/route.ts
@@ -1,8 +1,6 @@
 import {GoogleSpreadsheet} from 'google-spreadsheet'
 import { NextResponse } from 'next/server';
 import {JWT} from 'google-auth-library'
-import transformKeys from './Transform'
-
 
 
 const serviceAccountAuth = new JWT({
@@ -13,49 +11,34 @@ const serviceAccountAuth = new JWT({
     ],
   });
 
-export async function GET (req:Request,res:Response){
+export async function POST (req:Request,res:Response){
     try {
         
-        // const body = await req.json();
+        const body = await req.json();
 
-        let googleSheetId = "1HVHygjcTubZLlubR5KESZMjQAXTKRsNDzFV_-GBBNwg" ; 
-        let prefix = "MyTestingOrder-0";
-        let sheetId=0
-        let apiKey="mwxoz6wlowff4nj7ijwz1kgqxrpddwnfwolcbn3t"
-        let apiSecret="apjgalnbmcl0ddvriula0rwqyci6xdmat49jiyak"; 
-        let product="Testing Product"
-
+        let {googleSheetId,prefix,apiKey,apiSecret,product,sheetId} = body 
         
-        // if(!googleSheetId || !prefix ||!apiKey || !apiSecret || !product) { return NextResponse.json({success:false,message:"Enter all the fields"},{status:400}) };
+
+        if(!googleSheetId || !prefix ||!apiKey || !apiSecret || !product ) { return NextResponse.json({success:false,message:"Enter all the fields"},{status:400}) };
 
         let token : any = await fetch("https://api.codinafrica.com/api/users/apilogin",{method:"POST",body:JSON.stringify({key:apiKey,secret:apiSecret}),headers:{"Content-Type":"application/json"}});
         token = await token.json();
         
         if(!token?.content?.token){
-            return NextResponse.json({success:false,message:"Something went wrong while fetching token or Invalid API key or Secret make sure you enter correct"},{status:400})
+            return NextResponse.json({success:false,message:"Something went wrong while fetching token or Invalid code of africa API key or Secret make sure you enter correct"},{status:400})
         }
 
         token = token.content.token;
 
-        
-        
         let data = [];
 
-        const pattern = /^(\D+)-(\d+)$/;
+        const pattern = /^[a-zA-Z0-9]+-[a-zA-Z0-9]+-\d+$/;
         const match = prefix.match(pattern);
 
-        if(!match){return NextResponse.json({success:false,message:"The prefix should be the format of text-0"},{status:500})}
+        if(!match){return NextResponse.json({success:false,message:"The prefix should be the format of store-country-number e.g shopify-nigeria-0"},{status:500})}
 
-        const hyphenIndex = prefix.indexOf("-");
-        let number : number  = 0;
-        
-        if (hyphenIndex !== -1) {
-          const numberString :any = prefix.slice(hyphenIndex + 1);
-          if (!isNaN(numberString)) {
-            number = parseInt(numberString);
-          }
-        }
-
+        const [store,orderCountry,number] = prefix.split("-")
+       
         const doc = new GoogleSpreadsheet(googleSheetId,serviceAccountAuth);
         await doc.loadInfo();
 
@@ -63,80 +46,76 @@ export async function GET (req:Request,res:Response){
         if(!sheet) { return NextResponse.json({success:false,message:"Sheet Not found please see that if you have written correct sheet Id"},{status:500}) }
         
         const rows = await sheet.getRows();
-        let headerValues : string[] = [...sheet.headerValues,"items","total"];
+        
+        let i = number - 1;
 
-        if (hyphenIndex !== -1) {
-          prefix = prefix.slice(0, hyphenIndex + 1);
-        }
-        
-        let i = 0;
-        
         for (const row of rows) {
+        
             i++
             const rowData : any = {};
-        
-            headerValues.forEach((header,index) => {
-                let totalPrice = 0;
-                if(header=="Order ID" || header=="order id" || header === "Order id" || header === "order Id" || header === "order ID"){
-                    rowData[header] = `${prefix}${number+i}`;
-                }
 
-                else if(header==="items"){
-                  rowData[header] = [
-                    {
-                      name:product,
-                      code:row.get("SKU"),
-                      quantity:parseInt(row.get("Total quantity")),
-                      price:parseInt(row.get("Total charge"))
-                    }
-                  ]
-                  
-                }
-            
-                else if (header==="Total charge" || header==="Total quantity" || header==="SKU") {
-                }
+            rowData["orderId"] = row.get("order id") || row.get("Order ID") || row.get("order Id") || row.get("orderId") || row.get("OrderId");
+            if(!rowData["orderId"]){return NextResponse.json({success:false,message:"Order Id column not found and it is required make sure you have written it correct in google sheets"},{status:500})}
 
-                else{
-                  rowData[header] = row.get(header);
-                }
-            });
-        
+            rowData["orderId"] = `${store}-${orderCountry}-${i}`
+
+            rowData["source"] = row.get("product url") || row.get("Product URL") || row.get("product URL") || row.get("Product url") || row.get("productUrl") || row.get("productURL") || row.get("ProductUrl")
+            if(!rowData["source"]){return NextResponse.json({success:false,message:"Product URL column not found and it is required make sure you have written it correct in google sheets"},{status:500})}
+
+            rowData['fullName'] = row.get("Full name") || row.get("full name") || row.get("full Name") || row.get("fullName") || row.get("Fullname");
+            if(!rowData['fullName']) {return NextResponse.json({success:false,message:"Full Name column not found and it is required make sure you have written it correct in google sheets"},{status:500})}
+
+            rowData['phone'] = row.get("Phone") || row.get("phone");
+            if(!rowData['phone']) {return NextResponse.json({success:false,message:"Phone column not found and it is required make sure you have written it correct in google sheets"},{status:500})}
+
+    
+            rowData['city'] = row.get("City") || row.get("city");
+
+            rowData['items'] = [
+              {
+                name:product,
+                code:row.get("SKU") || row.get("sku") || row.get("Sku"),
+                quantity:parseInt(row.get("Total quantity") || row.get("Total Quantity") || row.get("total quantity") || row.get("Totalquantity") || row.get("totalQuantity")),
+                price:parseInt(row.get("Total charge") || row.get("total charge") || row.get("Totalcharge") || row.get("totalCharge") || row.get("total Charge"))
+              }
+            ]
+
+            if(!rowData["items"][0].code || !rowData['items'][0].quantity || !rowData['items'][0].price){return NextResponse.json({success:false,message:"items column not found and it is required make sure you have written it correct in google sheets"},{status:500})}
+
+            rowData['total'] = rowData["items"].reduce((a:number, b:any) => {
+              return a+b.price
+            },0)
+
             data.push(rowData);
-        
         }
-
-        data = data.map(item=>{
-
-           let totalPrice =  item.items.reduce((acc:number,currentValue:any)=>{
-            return acc+currentValue.price
-           },0)
-
-           return {...transformKeys(item),total:totalPrice}
-
+        
+        const promises = data.map(async object => {
+          const response = await fetch("https://api.codinafrica.com/api/orders/apicreate",{method:"POST",headers:{"Content-Type":"application/json","x-auth-token":token},body:JSON.stringify(object)});
+          return response.text();
         });
+    
+        const results = await Promise.all(promises);
+        let responseMessage = {} ;
+        let responseStatus = {};
 
-        // data = data.map(item=>toCamelCase(item))
-        
-        // let API = await fetch("https://api.codinafrica.com/api/orders/apicreate",{method:"POST",headers:{"Content-Type":"application/json","x-auth-token":token},body:JSON.stringify({
-        //   orderId:"testingorderId",
-        //   source:"testingsournce",
-        //   fullName:"testingfullName",
-        //   phone:"433434",
-        //   country:"testingcountry",
-        //   city:"testingcity",
-        //   address:"testingaddress",
-        //   items:[{code:"testingcode"}],
-        //   total:1
-        // })});
-        
-        // let order_id = await API.text();
+        results.forEach(result=>{
+          let check = JSON.parse(result);
+          
+          if(check?.content){
+            responseMessage = {success:false,message:check?.content};
+            responseStatus = {status:500}
+            return
+          }
 
-  
-        return NextResponse.json({success:true,message:data},{status:200});
+          responseMessage = {success:true,message:"Order Created Successfully"},
+          responseStatus = {status:200}
+        })
+       
+        return NextResponse.json(responseMessage,responseStatus);
         
 
     } catch (error:any) {
-        return NextResponse.json({success:false,error:error.message},{status:500})
+        return NextResponse.json({success:false,message:error.message},{status:500})
     }
 }
 
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,24 @@
 "use client"
 
-import { FileSpreadsheet, Key, ListOrdered, Lock, Sheet, ShoppingCart } from "lucide-react";
-import { Card } from "@/components/ui/card"
+import { FileSpreadsheet, Key, ListOrdered, Loader2, Lock, Sheet, ShoppingCart } from "lucide-react";
+import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import Loader from "@/components/Loader";
+import { useToast } from "@/components/ui/use-toast";
+import { cn } from "@/lib/utils";
 
 export default function Dashboard() {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const codeInAfricaAPIKey = useRef(null);
-  const productName = useRef(null);
-  const codeInAfricaSecretKey = useRef(null);
-  const googleSheetId = useRef(null);
-  const orderIdPrefix = useRef(null);
-  const sheetId = useRef(null);
+  const [codeInAfricaAPIKey, setCodeInAfricaAPIKey] = useState("");
+  const [productName, setProductName] = useState("");
+  const [codeInAfricaSecretKey, setCodeInAfricaSecretKey] = useState("");
+  const [googleSheetId, setGoogleSheetId] = useState("");
+  const [orderIdPrefix, setOrderIdPrefix] = useState("");
+  const [sheetId, setSheetId] = useState("");
 
   const inputs = [
     {
@@ -21,18 +26,20 @@ export default function Dashboard() {
       icon: Key,
       bgColor: "rgba(139, 92, 246,0.1)",
       color: "#8b5cf6",
-      label: "Code in africa API Key",
-      description: "code in africa api key can be found in your code africa dashboard setting",
-      ref: codeInAfricaAPIKey
+      label: "Code in Africa API Key",
+      description: "Code in Africa API key can be found in your Code Africa dashboard settings",
+      value: codeInAfricaAPIKey,
+      setter: setCodeInAfricaAPIKey,
     },
     {
       placeHolder: "Code Africa Secret Key",
       icon: Lock,
       bgColor: "rgba(16, 185, 129,0.1)",
       color: "#10b981",
-      label: "Code in africa Secret Key",
-      description: "code in africa secret key can be found in your code africa dashboard setting",
-      ref: codeInAfricaSecretKey
+      label: "Code in Africa Secret Key",
+      description: "Code in Africa secret key can be found in your Code Africa dashboard settings",
+      value: codeInAfricaSecretKey,
+      setter: setCodeInAfricaSecretKey,
     },
     {
       placeHolder: "Google Sheet ID",
@@ -40,8 +47,9 @@ export default function Dashboard() {
       bgColor: "rgba(190, 24, 93,0.1)",
       color: "#be185d",
       label: "Google Sheet ID",
-      description: "found at your google sheet URL docs.google.com/spreadsheets/d/YOUR_GOOGLE_SHEET_ID",
-      ref: googleSheetId
+      description: "Found at your Google Sheet URL docs.google.com/spreadsheets/d/YOUR_GOOGLE_SHEET_ID",
+      value: googleSheetId,
+      setter: setGoogleSheetId,
     },
     {
       placeHolder: "Sheet ID",
@@ -49,17 +57,19 @@ export default function Dashboard() {
       bgColor: "rgba(139, 92, 246,0.1)",
       color: "#c2410c",
       label: "Sheet ID",
-      description: "found at your google sheet URL spreadsheets/d/GOOGLE_SHEET_ID/edit#gid=YOUR_SHEET_ID",
-      ref: sheetId
+      description: "Found at your Google Sheet URL spreadsheets/d/GOOGLE_SHEET_ID/edit#gid=YOUR_SHEET_ID",
+      value: sheetId,
+      setter: setSheetId,
     },
     {
       placeHolder: "Product Name",
       icon: ShoppingCart,
       bgColor: "rgba(139, 92, 246,0.1)",
       color: "#c2410c",
-      label: "Product Names",
-      description: "Add your Product Name that is registerd in code in africa dashboard",
-      ref:productName
+      label: "Product Name",
+      description: "Add your Product Name that is registered in Code in Africa dashboard",
+      value: productName,
+      setter: setProductName,
     },
     {
       placeHolder: "Order ID Prefix",
@@ -67,16 +77,49 @@ export default function Dashboard() {
       bgColor: "rgba(139, 92, 246,0.1)",
       color: "#c2410c",
       label: "Order ID Prefix",
-      description: "Add your Order ID prefix in formate of order-7",
-      ref: orderIdPrefix,
-      
+      description: "Add your Order ID prefix in the format of order-7",
+      value: orderIdPrefix,
+      setter: setOrderIdPrefix,
     },
+  ];
 
-  ]
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  }
+    setIsLoading(true);
+
+    toast({ description:<div className="flex space-x-2"><p>Creating your order please wait </p><Loader2 className="animate-spin"/></div>, variant: "default"});
+
+    const formData = {
+      googleSheetId,
+      prefix: orderIdPrefix,
+      apiKey: codeInAfricaAPIKey,
+      apiSecret: codeInAfricaSecretKey,
+      product: productName,
+      sheetId,
+    };
+
+    try {
+      const response = await fetch('/api/getSheet', {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const API = await response.json();
+
+      if(API.success){
+        toast({title:"Created",description:API.message,className:"bg-green-500 text-white"});
+        setIsLoading(false);
+        return
+     }
+
+     toast({title:"Failed",description:API.message,variant:"destructive"});
+     setIsLoading(false);
+
+    } catch (error) {
+      toast({ title: "Error", description: "An error occurred during API call", variant: "destructive" });
+    } 
+  };
 
   return (
     <div>
@@ -85,30 +128,33 @@ export default function Dashboard() {
           Explore the power of Web
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Use our app to easily transfer you google sheets to codeinafrica
+          Use our app to easily transfer your Google Sheets to Code in Africa
         </p>
       </div>
-      <form onSubmit={handleSubmit} className="py-20 md:px-20 lg:px-32  grid lg:grid-cols-2 gap-y-10 lg:gap-x-4">
-        {inputs.map((input) => (
-          <Card key={input.placeHolder} className="p-4 border-black/5 max-w-full  flex flex-col items-center justify-between hover:shadow-md transition cursor-pointer">
-            <div className="flex items-center gap-x-4">
-              <div style={{ background: input.bgColor }} className={"p-2 w-fit rounded-md"}>
-                <input.icon color={input.color} className={"w-8 h-8"} />
+      <form onSubmit={handleSubmit} className={cn("py-20 md:px-20 lg:px-32  grid lg:grid-cols-2 gap-y-10 lg:gap-x-4", isLoading?"opacity-40":"")}>
+          {inputs.map((input) => (
+            <Card key={input.placeHolder} className="p-4 border-black/5 max-w-full  flex flex-col items-center justify-between hover:shadow-md transition cursor-pointer">
+              <div className="flex items-center gap-x-4">
+                <div style={{ background: input.bgColor }} className={"p-2 w-fit rounded-md"}>
+                  <input.icon color={input.color} className={"w-8 h-8"} />
+                </div>
+                <div className="font-semibold">
+                  {input.label}
+                </div>
               </div>
-              <div className="font-semibold">
-                {input.label}
+              <p className="md:text-sm w-full text-xs mt-5 break-words text-muted-foreground text-center leading-5">{input.description}</p>
+              <div className="mt-4">
+                <Input value={input.value} onChange={(e) => input.setter(e.target.value)} required placeholder={input.placeHolder} />
               </div>
-            </div>
-            <p className="md:text-sm w-full text-xs mt-5 break-words text-muted-foreground text-center leading-5">{input.description}</p>
-            <div className="mt-4">
-               <Input ref={input.ref} required placeholder={input.placeHolder} />
-            </div>
-          </Card>
-        ))}
-        <div className="flex lg:col-span-2 justify-center">
-          <Button className="lg:flex-[0.5]">Submit</Button>
-        </div>
-      </form>
+            </Card>
+          ))}
+          <div className="flex lg:col-span-2 justify-center">
+            <Button disabled={isLoading} className="lg:flex-[0.5]">
+              {isLoading ? <Loader2 className="animate-spin" /> : "Create"}
+            </Button>
+          </div>
+        </form>
+      
     </div>
   )
 }
