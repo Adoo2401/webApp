14719b0faa02373a7b33e8723f11d13e3f7525af
Adoo2401 <abdullahrehmani230@gmail.com>
2023-08-21 16:34:44 +0500

    check8

--- app/api/scheduler/route.ts
+++ app/api/scheduler/route.ts
@@ -10,16 +10,17 @@ export async function GET(req: NextRequest) {
     
       await mongoose.connect(process.env.MONGODB_URL!);
       let products = await Product.find();
+      console.log("🚀 ~ file: route.ts:13 ~ GET ~ products:", products)
 
-      for (let i = 0; i < products.length; i++) {
-        if (products[i].cronJobTiming) {
-          await automate(products[i].name);
-        } else {
-          continue;
-        }
-      }
+      // for (let i = 0; i < products.length; i++) {
+      //   if (products[i].cronJobTiming) {
+      //     await automate(products[i].name);
+      //   } else {
+      //     continue;
+      //   }
+      // }
     
-    return NextResponse.json({ success: true, message: "recieved" });
+    return NextResponse.json({ success: true, message: products });
   } catch (error: any) {
     return NextResponse.json(
       { success: false, message: error.message },
