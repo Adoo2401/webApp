a2e8f0a5b2130eb5f32fa8d85198a8c0f6e2cc35
Adoo2401 <abdullahrehmani230@gmail.com>
2023-08-21 16:43:04 +0500

    check10

--- app/api/scheduler/route.ts
+++ app/api/scheduler/route.ts
@@ -20,7 +20,7 @@ export async function GET(req: NextRequest) {
         }
       }
     
-    return NextResponse.json({ success: true, message: check });
+    return NextResponse.json({ success: true, message: products });
   } catch (error: any) {
     return NextResponse.json(
       { success: false, message: error.message },
